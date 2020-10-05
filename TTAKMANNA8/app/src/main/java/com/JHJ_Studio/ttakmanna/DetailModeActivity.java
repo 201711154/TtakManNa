8362e72b82c3512cd9eb9ef0dfb8129ac17a7581
JJW <43820253+201711154@users.noreply.github.com>
2020-10-05 21:28:55 +0900

    AddressSearch(ing-1)
    역 지오코딩으로 주소로 지명 검색 구현중
    (뭐 건드린다길래 후딱 올린다!!)

--- TTAKMANNA8/app/src/main/java/com/JHJ_Studio/ttakmanna/DetailModeActivity.java
+++ TTAKMANNA8/app/src/main/java/com/JHJ_Studio/ttakmanna/DetailModeActivity.java
@@ -1,6 +1,9 @@
 package com.JHJ_Studio.ttakmanna;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -16,13 +19,17 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.IdRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import net.daum.mf.map.api.MapView;
 
+import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.time.DayOfWeek;
+import java.util.List;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 //새 일정 - 세부사항화면
@@ -35,6 +42,11 @@ public class DetailModeActivity extends AppCompatActivity {
     private RadioGroup checkModeGroup; // 불가능한 날짜인지 가능한 날짜인지
     private CheckBox[] dayOfWeek = new CheckBox[7]; // 요일 박스
     private int[] week = {R.id.monday, R.id.tuesday, R.id.wednesday, R.id.thursday, R.id.friday, R.id.saturday, R.id.sunday};
+
+    // 지도 관련
+    EditText address;
+    MapView mapView;
+    Button address_search_button;
 
     // 얻은 데이터
     private boolean is_it_possible_day = false; // 가능한 요일인지 불가능한 요일인지 확인
@@ -53,6 +65,11 @@ public class DetailModeActivity extends AppCompatActivity {
         }
     }
 
+    // 데이터가 전부 제대로 입력되었는지 검증하는 용
+    boolean didParticipationNameInput = false;
+    boolean didPosInput = false;
+    boolean didDateSet = false;
+
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,19 +84,6 @@ public class DetailModeActivity extends AppCompatActivity {
         // 입력 정보
         EditText name = (EditText)findViewById(R.id.nickname);
 
-        // 불가능한 요일인지 가능한 요일인지 체크
-        checkModeGroup = (RadioGroup)findViewById(R.id.pos_impos);
-        checkModeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.impossible){
-                    is_it_possible_day = false;
-                }
-                else{
-                    is_it_possible_day = true;
-                }
-            }
-        });
 
         // 무슨 요일이 가능한지 체크 - 미안 for문으로 줄여두려고 했는데 안되더라 가독성은... 알아서 봐줘 S2
         dayOfWeek[0] = (CheckBox) findViewById(week[0]);
@@ -160,11 +164,57 @@ public class DetailModeActivity extends AppCompatActivity {
             }
         });
 
-        // 위치 정보 받아옴
-       MapView mapView = new MapView(this);
 
+        // 맵 뷰 띄우기
+       mapView = new MapView(this);
        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.mapView);
        mapViewContainer.addView(mapView);
+       // 주소
+       address = (EditText) findViewById(R.id.address);
+       // 주소 검색 버튼을 누르면 주소를 기반으로 geocoding 후
+       // 주어진 좌표들 중 select 하고 해당 좌표로 맵 이동
+       address.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               List<Address> pos_searched; // 좌표들을 집어넣을 리스트
+               final Address pos_confirmed; // 그 중 골라진 좌표
+
+               // geocording
+               Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+               String address_str = address.toString();
+               try {
+                   pos_searched = geocoder.getFromLocationName(address_str, 15);
+               } catch (IOException e) {
+                   // 결과물을 찾지 못했을 경우 메세지 띄움
+                   Toast.makeText(getApplicationContext(), "주소 검색 결과가 존재하지 않습니다.", Toast.LENGTH_LONG);
+                   // e.printStackTrace();
+                   // 이벤트 종료
+                   return;
+               }
+
+               // 역 지오코딩으로 얻은 좌표 한글로 변환
+               // 기능 추가해야 함
+               final CharSequence[] poss = {"여기에 한글 변환한 좌표 넣어야 하는데 시간이 부족"};
+
+               // 팝업창에 좌표 리스트 선택하게 함
+               // 참고: https://mixup.tistory.com/36
+               AlertDialog.Builder select_pos_popup = new AlertDialog.Builder(DetailModeActivity.this,
+                       android.R.style.Theme_DeviceDefault_Light_Dialog_Alert); //context 수정했음 확인 필요
+
+               select_pos_popup.setTitle("원하는 위치를 선택하세요")
+                       .setItems(poss, new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialog, int which) {
+                               Toast.makeText(getApplicationContext(),
+                                       poss[which], Toast.LENGTH_LONG).show();
+                           }
+                       })
+                       .setCancelable(false)
+                       .show();
+
+           }
+       });
+
 
         // 시간 정보 받아옴
         time_from = (TimePicker) findViewById(R.id.time_from);
